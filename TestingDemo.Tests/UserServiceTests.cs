7354557baf55902aeba991148b2106907359a4ea
Eric Lobdell <elobdell@rediker.com>
2018-01-11 15:04:22 -0500

    add updated files

---
+++ TestingDemo.Tests/UserServiceTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using TestingDemo.Services;
+using TestingDemo.Tests.mocks;
+using Xunit;
+
+namespace TestingDemo.Tests
+{
+    public class UserServiceTests
+    {
+        MockUserRepository _userRepository;
+
+        public UserServiceTests()
+        {
+            _userRepository = new MockUserRepository();
+        }
+
+        [Fact]
+        public void ChangeName_requires_first_name()
+        {
+            var invalidFirstName = string.Empty;
+            var validLastName = "nont empty";
+
+            var sut = new UserService(_userRepository);
+            var ex = Assert.Throws<ArgumentException>(() => sut.ChangeName(MockEntities.User.Id, invalidFirstName, validLastName));
+
+            Assert.Contains("First name required", ex.Message);
+        }
+
+        [Fact]
+        public void ChangeName_does_not_call_repo_if_when_first_name_omitted()
+        {
+            var invalidFirstName = string.Empty;
+            var validLastName = "not empty";
+
+            var sut = new UserService(_userRepository);
+            var ex = Assert.Throws<ArgumentException>(() => sut.ChangeName(MockEntities.User.Id, invalidFirstName, validLastName));
+
+            Assert.False(_userRepository.GetWasCalled);
+        }
+
+        [Fact]
+        public void ChangeName_returns_null_when_save_fails()
+        {
+            var validFirstName = "Rosco P.";
+            var validLastName = "Coltrane";
+
+            _userRepository.ThrowOnSave = true;
+
+            var sut = new UserService(_userRepository);
+            var result = sut.ChangeName(MockEntities.User.Id, validFirstName, validLastName);
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void ChangeName_returns_user_with_updated_name_when_valid()
+        {
+            var validFirstName = "Boss";
+            var validLastName = "Hogg";
+
+            var sut = new UserService(_userRepository);
+            var result = sut.ChangeName(MockEntities.User.Id, validFirstName, validLastName);
+
+            Assert.Equal(MockEntities.User.Id, result.Id);
+            Assert.Equal(validFirstName, result.FirstName);
+            Assert.Equal(validLastName, result.LastName);
+        }
+    }
+}
---
+++ TestingDemo/Interfaces/IRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace TestingDemo.Interfaces
+{
+    public interface IRepository<T>
+    {
+        T Get(int id);
+        void Save(T entity);
+    }
+}
